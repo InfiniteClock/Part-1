b1b8ea9a5bfd43973eb56687860866132ee838ca
Bryce Davie <daviebry@shernet.sheridancollege.ca>
2024-01-10 15:36:21 -0500

    Week 1 Task 4b complete: Turret Rotates with vertical key inputs

--- Assets/Week 1/Scripts/Turret.cs
+++ Assets/Week 1/Scripts/Turret.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Missile : MonoBehaviour
+public class Turret : MonoBehaviour
 {
     public float speed = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+        float keyboardInput = Input.GetAxis("Vertical");
+        transform.Rotate(transform.forward, keyboardInput * speed * Time.deltaTime);
     }
 }
